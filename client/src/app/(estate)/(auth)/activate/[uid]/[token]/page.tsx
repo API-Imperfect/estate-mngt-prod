81210690b971439c8bedc1ce7549e23de3cb05a5
API-Imperfect <alphaogilo@gmail.com>
2024-05-11 14:43:50 +0200

    feat: add activation page

--- client/src/app/(estate)/(auth)/activate/[uid]/[token]/page.tsx
+++ client/src/app/(estate)/(auth)/activate/[uid]/[token]/page.tsx
@@ -1,7 +1,61 @@
-export default function ActivationPage() {
+"use client";
+
+import { useActivateUserMutation } from "@/lib/redux/features/auth/authApiSlice";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+
+interface ActivationProps {
+	params: {
+		uid: string;
+		token: string;
+	};
+}
+
+export default function ActivationPage({ params }: ActivationProps) {
+	const router = useRouter();
+	const [activateUser, { isLoading, isSuccess, isError, error }] =
+		useActivateUserMutation();
+
+	useEffect(() => {
+		const { uid, token } = params;
+		activateUser({ uid, token });
+	}, [activateUser, params]);
+
+	useEffect(() => {
+		if (isSuccess) {
+			toast.success("Account activated successfully!");
+			router.push("/login");
+		} else if (isError && error) {
+			toast.error("Failed to activate your account");
+		}
+	}, [isSuccess, isError, error, router]);
+
 	return (
-		<div>
-			<h1 className="dark:text-pumpkin text-6xl">Activation Page</h1>
+		<div className="flex min-h-screen items-center justify-center">
+			<div className="text-center">
+				<h3 className="dark:text-platinum font-robotoSlab text-2xl font-bold text-gray-800 sm:text-4xl md:text-5xl">
+					{isLoading ? (
+						<div className="flex-center">
+							<span className="mr-2">⏰</span>
+							<span>Activating your account....Please wait</span>
+							<span className="ml-2">🥱</span>
+						</div>
+					) : isSuccess ? (
+						<div>
+							<span className="mr-2">✅</span>
+							<span>Account Activated successfully!</span>
+						</div>
+					) : (
+						isError && (
+							<div>
+								<span className="mr-2">❌</span>
+								<span>Your account has already been activated...</span>
+							</div>
+						)
+					)}
+				</h3>
+			</div>
 		</div>
 	);
 }
